6a7569082d043bb9c9c2fff7c64b575e1463b26e
Ian <billknye@gmail.com>
2021-12-14 20:58:59 -0500

    Day 7, but slow

---
+++ src/AoCBlazor/Pages/2021/Day7Part1.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace AoCBlazor.Pages._2021;
+
+[Route("/2021/Day7/Part1")]
+public class Day7Part1 : ConsoleComponentBase
+{
+    protected override async Task Main()
+    {
+        Console.WriteLine("2021 - Day 7 - Part 1", ConsoleColor.DarkMagenta);
+        Console.WriteLine("Paste or drag input data");
+
+        var data = await Console.ReadLine();
+
+        Console.WriteLine("Running...");
+
+        await Task.Delay(1);
+
+        var horizonalPositions = data.Split(',').Select(n => int.Parse(n));
+
+        var max = horizonalPositions.Max();
+
+        var min = Enumerable
+            .Range(0, max)
+            .Select(n => (Position: n, Sum: horizonalPositions.Sum(m => Math.Abs(n - m))))
+            .OrderBy(n => n.Sum).First();
+
+        Console.WriteLine($"Result Position: {min.Position}, Total: {min.Sum}");
+
+    }
+}
---
+++ src/AoCBlazor/Pages/2021/Day7Part2.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace AoCBlazor.Pages._2021;
+
+[Route("/2021/Day7/Part2")]
+public class Day7Part2 : ConsoleComponentBase
+{
+    protected override async Task Main()
+    {
+        Console.WriteLine("2021 - Day 7 - Part 2", ConsoleColor.DarkMagenta);
+        Console.WriteLine("Paste or drag input data");
+
+        var data = await Console.ReadLine();
+
+        Console.WriteLine("Running...");
+
+        await Task.Delay(1);
+
+        var horizonalPositions = data.Split(',').Select(n => int.Parse(n));
+
+        var max = horizonalPositions.Max();
+
+        var min = Enumerable
+            .Range(0, max)
+            .Select(n => (Position: n, Sum: horizonalPositions.Sum(m => GetFuelCost(n, m))))
+            .OrderBy(n => n.Sum).First();
+
+        Console.WriteLine($"Result Position: {min.Position}, Total: {min.Sum}");
+
+    }
+
+    public static int GetFuelCost(int start, int end)
+    {
+        var total = 0;
+        for (int i = 0; i < Math.Abs(end - start); i++)
+        {
+            total += (i + 1);
+        }
+
+        return total;
+    }
+}
